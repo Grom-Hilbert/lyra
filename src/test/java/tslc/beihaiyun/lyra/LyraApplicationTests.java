ff40f76ef6e24857a30de8f0c69a1c4bbea89f6a
skyfrost <skyfrost1024@outlook.com>
2025-07-19 13:24:11 +0800

    项目初始化

---
+++ src/test/java/tslc/beihaiyun/lyra/LyraApplicationTests.java
@@ -0,0 +1,13 @@
+package tslc.beihaiyun.lyra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
